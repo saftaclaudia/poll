66f1c44986c2a176ec098ce353c9acb9de3f6dc7
saftaclaudia <saftaclaudia27@yahoo.com>
2014-12-17 20:06:08 +0200

    refactor and create functions
    refactor addQuestion function and create addEditAnswer and
    removeEditAnswer function

--- function.js
+++ function.js
@@ -8,24 +8,43 @@ var ViewModel = {
 		type: ko.observable('multiple'),
 		answers: ko.observableArray([new Answer('')]),
 	},
-
+	
 	addNewAnswer: function(){
 		this.newQuestion.answers.push(new Answer(''));
 	},
 	removeAnswer: function(item){
 		ViewModel.newQuestion.answers.remove(item);
 	},
-	addQuestion: function () {
-		var newQuestion = {
-			question: this.newQuestion.question(),
-			type: this.newQuestion.type(),
-			answers: this.newQuestion.answers()
-		};
-		this.questions.push(newQuestion);
+
+	addEditAnswer: function(){
+		var answer = new Answer('');
+		answer.name= ko.observable('');
+		this.answers.push(answer);
 	},
+	removeEditAnswer: function(item){
+		this.remove(item);
+	},
+
+	addQuestion: function () {
+		var answersClone = [];
+		var answersClone = this.newQuestion.answers().map(function(item) {
+			return {
+				name: ko.observable(item.name)
+			};
+		});
+
+		var newQuestion = {
+			question: ko.observable (this.newQuestion.question()),
+			type: ko.observable(this.newQuestion.type()),
+			answers: ko.observableArray(answersClone)
+		};
+			this.questions.push(newQuestion);
+	},
+
 	removeQuestion: function(item){
 		ViewModel.questions.remove(item);
 	},
+	
 	edit: ko.observable(false),
 	editQuestion: function(){
 		ViewModel.edit(true);
@@ -33,6 +52,10 @@ var ViewModel = {
 	cancelEdit:function(){
 		ViewModel.edit(false);
 	},
+
+	saveEdit: function(){
+
+	}
 	
 };
 
--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 							</div>
 							<!--/ko -->
 							<div class="col-sm-offset-2">
-								<button type="button" class="btn btn-link" data-bind="click: addNewAnswer">
+								<button type="button" class="btn btn-link" data-bind="click:addNewAnswer">
 									<span class="glyphicon glyphicon-plus"></span>
 									Add new Choice
 								</button>
@@ -86,11 +86,12 @@
 				</div>
 				<div class="form-group">
 					<b class="col-sm-2 control-label">Answer:</b>
-					<ul class="col-sm-10">
+					<ul class="col-sm-10" >
 					<!-- ko foreach: answers-->
 						<li data-bind="text:name"></li>
 					<!-- /ko-->
 					</ul>
+					
 				</div>
 			</div>
 
@@ -99,7 +100,7 @@
 					<div class="form-group">
 						<label for="" class="col-sm-2 control-label"><span class="circle" data-bind="text: $index()+1"></span>Question: </label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" data-bind= "value:question" id="">
+							<input type="text" class="form-control" data-bind= "value:question" id="newQuestion">
 						</div>
 					</div>
 					<div class="form-group">
@@ -107,18 +108,18 @@
 					<div class="col-sm-10">
 						<div class="radio">
 							<label>
-								<input type="radio" name="type" value="yesno" data-bind= "checked: newQuestion.type">
+								<input type="radio" name="type" value="yesno" data-bind= "checked:type">
 								Yes/No form
 							</label>
 						</div>
 						<div class="radio">
 							<label>
-								<input type="radio" name="type" value="multiple" data-bind="checked: newQuestion.type">
+								<input type="radio" name="type" value="multiple" data-bind="checked:type">
 								Multiple Choice form
 							</label>
 						</div>
-						<div data-bind ="visible: ($root.newQuestion.type() == 'multiple')">
-							<!-- ko foreach: $root.newQuestion.answers -->
+						<div data-bind ="visible: (type() == 'multiple')">
+							<!-- ko foreach: answers -->
 							<div class="form-group">
 								<label class="col-sm-2 control-label">
 									<div class="text-left">Choise #<span data-bind="text: $index()+1"></span></div>
@@ -127,7 +128,7 @@
 									<div class="input-group">
 										<input type="text" class="form-control" data-bind="value:name">
 										<span class="input-group-btn">
-											<button class="btn btn-default" data-bind="click: $root.removeAnswer">
+											<button class="btn btn-default" data-bind="click: $root.removeEditAnswer.bind($parent.answers)">
 												<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
 											</button>
 										</span>
@@ -136,7 +137,7 @@
 							</div>
 							<!--/ko -->
 							<div class="col-sm-offset-2">
-								<button type="button" class="btn btn-link" data-bind="click: $root.addNewAnswer">
+								<button type="button" class="btn btn-link" data-bind="click: $root.addEditAnswer">
 									<span class="glyphicon glyphicon-plus"></span>
 									Add new Choice
 								</button>
@@ -147,9 +148,6 @@
 									<button type='button' class='btn btn-link' data-bind="click: $root.cancelEdit"> Cancel</button>
 								</div>
 							</div>	
-
-
-
 						</div>
 					</div>
 				</div>
